2a5c2dfff15145d8e028b329eceda6c9df67aa09
Ruiru11 <erastusruiru11gmail.com>
2018-10-24 13:08:06 +0300

    chore-[finishes #161444604] add init file

---
+++ app/models/tables.py
@@ -0,0 +1,35 @@
+h1 = (
+    """
+CREATE TABLE IF NOT EXISTS PRODUCTS(
+name VARCHAR(20) NOT NULL UNIQUE,
+description VARCHAR(30) NOT NULL,
+price INTEGER NOT NULL,
+category VARCHAR(50) NOT NULL,
+product_id VARCHAR PRIMARY KEY
+)
+""")
+
+h3 = (
+    """
+CREATE TABLE IF NOT EXISTS SALES(
+sale_id VARCHAR PRIMARY KEY NOT NULL,
+user_id VARCHAR ,
+cost INTEGER NOT NULL,
+description VARCHAR NOT NULL,
+FOREIGN KEY(user_id) REFERENCES users(id)
+
+
+)
+""")
+
+h2 = ("""
+CREATE TABLE IF NOT EXISTS USERS(
+id VARCHAR primary key,
+email VARCHAR(20) NOT NULL UNIQUE,
+password VARCHAR NOT NULL,
+role VARCHAR(20) NOT NULL
+)
+""")
+
+
+commands = [h1, h2, h3]
