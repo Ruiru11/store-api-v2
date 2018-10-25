fe3d56e9d5feb0e1fe8113c4b465f534d936aef7
Ruiru11 <erastusruiru11gmail.com>
2018-10-25 12:16:51 +0300

    feature-[finishes #161382996]admin can delete product

--- app/controllers/products_controllers.py
+++ app/controllers/products_controllers.py
@@ -31,3 +31,44 @@ class Items(object):
 
         except (Exception, psycopg2.DatabaseError) as error:
             print("ERROR inserting into products", error)
+
+    def get_items(self):
+        """
+        The function to get all products created.
+
+        Returns:
+            products: A list of all created products.
+        """
+        self.connection.cursor.execute(
+            """SELECT * FROM products"""
+        )
+        item = self.connection.cursor.fetchall()
+        return(jsonify(item))
+
+    def get_item(self, id):
+        """
+        The function to gets a specific product using its unique id.return.
+
+        Parameters:
+            id: This is the identiication number of product to be viewed.
+
+        Returns:
+            Products: A single product
+
+
+        """
+        self.connection.cursor.execute(
+            "SELECT * FROM  products WHERE product_id=%s", [id]
+        )
+        item = self.connection.cursor.fetchone()
+        return(make_response(jsonify(item)))
+
+    def delete_product(self, id):
+        self.connection.cursor.execute(
+            "DELETE FROM products WHERE product_id=%s", [id]
+        )
+        response_object = {
+            "satus": "pass",
+            "message": "order deleted"
+        }
+        return(make_response(jsonify(response_object)))
