ad4debf3f2e11f14bdb308b2b3b982e66b0b0dad
SnyderMbishai <csmbishai@gmail.com>
2018-10-25 15:16:50 +0300

    chore-[finishes #161477516] setup documentation

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import os
 from app import create_app
+from flasgger import Swagger
 
 app = create_app(os.getenv('ENV'))
+
+Swagger(app, template_file="docs.yml")
 
 if __name__ == '__main__':
     app.run()
